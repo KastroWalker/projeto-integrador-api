b73c1e0687904265fd9034caf6e444b1e7802eb9
KastroWalker <victorcsa2002@gmail.com>
2021-09-26 19:19:06 -0300

    configurando node

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Express from 'express';
+
+const app = Express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server is run at http://localhost:${port}`);
+});
