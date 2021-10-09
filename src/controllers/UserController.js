9bb2ace0ca6acfc4fd162eab3882045482ce9853
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-08 22:16:44 -0300

    Alterações para adicionar o relacionamento entre as tabelas

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,8 +3,14 @@ import UserService from '../services/UserService';
 class UserController {
   async createNewUser(req, res) {
     try {
-      const data = req.body;
-      const newUser = await UserService.insert(data);
+      const { name, username, password } = req.body;
+      const { type_id } = req.params;
+      const newUser = await UserService.insert({
+        type_id,
+        name,
+        username,
+        password,
+      });
       res.status(201).json(newUser);
     } catch (err) {
       res.status(400).send(err);
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,11 +3,16 @@ import UserRepository from '../repositories/UserRepository';
 class UserService {
   async insert(data) {
     try {
-      const { name, username, password } = data;
+      const { name, username, password, type_id } = data;
       if (!name || !username || !password) {
         throw new Error('You should provider a name, username, and password');
       }
-      const newUser = await UserRepository.insert({ name, username, password });
+      const newUser = await UserRepository.insert({
+        name,
+        username,
+        password,
+        type_id,
+      });
       return newUser;
     } catch (err) {
       throw err;
