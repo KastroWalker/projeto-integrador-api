8fcfc6c94a53d0044ad1671567643723e86bdb7f
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-12 19:03:52 -0300

    Utilizando bcrypt para criptografar as senhas dos usuários

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,4 +1,5 @@
 import UserRepository from '../repositories/UserRepository';
+import bcrypt from 'bcrypt';
 
 class UserService {
   async insert(data) {
@@ -7,14 +8,20 @@ class UserService {
       if (!name || !username || !password) {
         throw new Error('You should provider a name, username, and password');
       }
+
       const userAlreadyExists = await UserRepository.searchByUsername(username);
       if (userAlreadyExists) {
         throw new Error('This username already exists');
       }
+
+      const salt = bcrypt.genSaltSync(12);
+      const hash = bcrypt.hashSync(password, salt);
+      console.log(hash);
+
       const newUser = await UserRepository.insert({
         name,
         username,
-        password,
+        password: hash,
         type_id,
       });
       return newUser;
