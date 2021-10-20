cd4b4ef22c3f923837c62a1924a66ad601f06717
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-20 08:56:44 -0300

    migration de lojas

---
+++ src/database/migrations/20211020111946-createStore.js
@@ -0,0 +1,26 @@
+export default {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('stores', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: { type: Sequelize.DataTypes.STRING, allowNull: false },
+      description: { type: Sequelize.DataTypes.STRING, allowNull: false },
+      status: { type: Sequelize.DataTypes.BOOLEAN, allowNull: false },
+      merchant_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'merchants', key: 'id' },
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('clients');
+  },
+};
