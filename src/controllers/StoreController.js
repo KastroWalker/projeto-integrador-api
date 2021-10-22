e7c8dc488b9e0c11e6a2f718e03e7cb520201085
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-21 21:31:22 -0300

    funções para atualizar a loja

--- src/controllers/StoreController.js
+++ src/controllers/StoreController.js
@@ -5,13 +5,25 @@ class StoreController {
     try {
       const { merchant_id } = req.params;
       const { name, description, status } = req.body;
-      const newStore = await StoreService.insert({
+      const newStore = await StoreService.insert(
         merchant_id,
         name,
         description,
-        status,
-      });
+        status
+      );
       res.status(201).json(newStore);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async update(req, res, next) {
+    const { id, merchant_id } = req.params;
+    const { name, description, status } = req.body;
+
+    try {
+      await StoreService.update(id, name, description, status, merchant_id);
+      return res.status(200).json({ name, description, status });
     } catch (err) {
       next(err);
     }
