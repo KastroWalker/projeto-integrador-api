be4fe0f91eceabfc3a0f04b1dc6cbe8dd9c2a951
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-26 09:58:36 -0300

    model de endereços

---
+++ src/models/AddressModel.js
@@ -0,0 +1,22 @@
+import { DataTypes, Model } from 'sequelize';
+import connection from '../database';
+import StoreModel from './StoreModel';
+
+class Address extends Model {}
+
+Address.init(
+  {
+    road: { type: DataTypes.STRING, allowNull: false },
+    number: { type: DataTypes.STRING, allowNull: false },
+    district: { type: DataTypes.STRING, allowNull: false },
+    city: { type: DataTypes.STRING, allowNull: false },
+    zipCode: { type: DataTypes.STRING, allowNull: false },
+    complement: { type: DataTypes.STRING, allowNull: true },
+    coordinate: { type: DataTypes.STRING, allowNull: true },
+  },
+  { sequelize: connection, modelName: 'Address', tableName: 'addresses' }
+);
+
+Address.hasOne(StoreModel, { foreignKey: 'addressId', as: 'store' });
+
+export default Address;
