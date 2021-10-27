169238a6f50a7db309caa4e476c7cf0113d6980e
Kauanphbbb <kauanphbbb@gmail.com>
2021-10-26 21:04:41 -0300

    Renomeando rotas para seguir um padrão

--- src/routes/stores.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import StoreController from '../controllers/StoreController';
-
-const routes = new Router();
-
-routes.post('/stores', StoreController.insert);
-
-export default routes;
