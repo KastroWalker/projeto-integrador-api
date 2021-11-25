c5b7f8d243839a5fa9d088c13f6dcdf5adc2af98
Kauanphbbb <kauanphbbb@gmail.com>
2021-11-25 18:33:56 -0300

    migration de categorias (pre-requisito para criar um produto)

---
+++ src/database/migrations/20211125205601-createFoodCategory.js
@@ -0,0 +1,28 @@
+export default {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('foodCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DataTypes.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('foodCategories');
+  },
+};
